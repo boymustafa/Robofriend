5fceaa1e4154f2386783b013fb8584b64bd9dae2
Boy_Nurah_Mustafa <boynurahmustafa@airasia.com>
2023-05-19 07:19:45 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Hello from './Hello';
+import Card from './components/Card';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from "./robot";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    {/*  <Hello greeting={'Hello' + 'React Ninja'}/>*/}
+    {/*  <div>*/}
+    {/*      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>*/}
+    {/*      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>*/}
+    {/*      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>*/}
+    {/*  </div>*/}
+    {/*    <CardList robots={robots}/>*/}
+      <App/>
+
   </React.StrictMode>
 );
 
